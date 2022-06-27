1d23a6b844267fda62847f488c1f09dc6f70bc5c
Minhyeong Yu <12ymh12@gmail.com>
2022-06-27 14:46:13 +0900

    Update train.py

--- train.py
+++ train.py
@@ -568,9 +568,10 @@ def qat_train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp di
         quantized_model.load_state_dict(csd, strict=False)  # load
         LOGGER.info(f'Transferred {len(csd)}/{len(quantized_model.state_dict())} items from {weights}')  # report
     
-    amp = check_amp(quantized_model)  # check AMP
+    # amp = check_amp(quantized_model)  # check AMP
 
     # Freeze
+    freeze = opt.freeze
     freeze = [f'quantized_model.{x}.' for x in (freeze if len(freeze) > 1 else range(freeze[0]))]  # layers to freeze
     for k, v in quantized_model.named_parameters():
         v.requires_grad = True  # train all layers
