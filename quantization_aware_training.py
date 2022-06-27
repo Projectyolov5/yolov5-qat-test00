b004883a00aada6f0d63b14368de6ddeab676d03
Minhyeong Yu <12ymh12@gmail.com>
2022-06-27 12:57:45 +0900

    Update quantization_aware_training.py

--- quantization_aware_training.py
+++ quantization_aware_training.py
@@ -279,13 +279,11 @@ def model_equivalence(model_1, model_2, device, rtol=1e-05, atol=1e-08, num_test
         
         Y1 = model_1(x)
         Y2 = model_2(x)
-        for y1, y2 in zip(Y1, Y2):
+        for i, (y1, y2) in enumerate(zip(Y1, Y2)):
             y1 = y1.detach().cpu().numpy()
             y2 = y2.detach().cpu().numpy()
             if np.allclose(a=y1, b=y2, rtol=rtol, atol=atol, equal_nan=False) == False:
-                print("Model equivalence test sample failed: ")
-                print(y1)
-                print(y2)
+                print("Model equivalence test sample failed: ", i)
                 return False
 
     return True
