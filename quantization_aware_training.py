1187c5e79a0cd18669b67233f856f6387f869e7f
Minhyeong Yu <12ymh12@gmail.com>
2022-06-27 12:47:17 +0900

    test

--- quantization_aware_training.py
+++ quantization_aware_training.py
@@ -276,6 +276,7 @@ def model_equivalence(model_1, model_2, device, rtol=1e-05, atol=1e-08, num_test
 
     for _ in range(num_tests):
         x = torch.rand(size=input_size).to(device)
+        print(len(model_1(x)))
         y1 = model_1(x).detach().cpu().numpy()
         y2 = model_2(x).detach().cpu().numpy()
         if np.allclose(a=y1, b=y2, rtol=rtol, atol=atol, equal_nan=False) == False:
