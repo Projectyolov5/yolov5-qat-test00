31da119f20dff98bce456d05e7005998ec53ebaf
Minhyeong Yu <12ymh12@gmail.com>
2022-06-27 16:58:14 +0900

    Update train.py

--- train.py
+++ train.py
@@ -620,8 +620,10 @@ def qat_train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp di
     print("Training QAT Model...")
     quantized_model.to(device)
 
+    print(quantized_model)
+
     # Image size
-    gs = max(int(quantized_model.model.stride.max()), 32)  # grid size (max stride)
+    gs = max(int(quantized_model.stride.max()), 32)  # grid size (max stride)
     imgsz = check_img_size(opt.imgsz, gs, floor=gs * 2)  # verify imgsz is gs-multiple
 
     # Batch size
